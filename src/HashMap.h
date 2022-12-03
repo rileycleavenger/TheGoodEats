79c872af9fc03fc531dd9abd2c69462427a1b62f
rgorman00 <gorman.ryan01@gmail.com>
2022-12-03 14:50:10 -0500

    adding changes to Hashmap files

--- src/HashMap.h
+++ src/HashMap.h
@@ -1,86 +1,43 @@
-
-//#ifndef PROJECT3_RESTAURANT_H
-//#define PROJECT3_RESTAURANT_H
-/*
-class Restaurant{
-private:
-
-    // class varaibles
-    string name;
-    string state;
-    string county;
-    string foodType;
-    vector<string> hours;
-    float longitude;
-    float latitude;
-
-public:
-
-    // getters
-    string getName();
-    string getState();
-    string getCounty();
-    string getFoodType();
-    vector<string> getHours();
-    float getLongitude();
-    float getLatitude();
-
-    // setters
-    void setName(string name_);
-    void setState(string state_);
-    void setCounty(string county_);
-    void setFoodType(string foodType_);
-    void setHours(vector<string> hours_);
-    void setLongitude(string longitude_);
-    void setLatitude(string latitude_);
-
-    // constructors
-    Restaurant();
-    Restaurant(string name_, string state_, string county_, string foodType_, vector<string> hours_, string longitude_, string latitude_);
-};
-*/
 #include <list>
 #include "Restaurant.h"
+#include <vector>
 
 class HashMap{
     private:
         // class variables
-        node(float K, Restaurant R) **table;
-        F hashFunc;
+        bucket bucketList[64800]; // array of buckets (each bucket hashcode is it's index in the array)
     public:
         // getters
-        bool find(float key1, float key2, Restaurant R);
+        std::vector<Restaurant> find(string state, string county); // returns a vector of restuarants in the specified state/county
 
         // setters
-        void insert(float key1, float key2, Restuarant R);
-        void removeNode(float key1, float key2);
+        void insert(Restaurant R); // inserts a new restaurant into the hashmap
 
         // constructors
         HashMap();
 
         // destructors
         ~HashMap();
-        hashFunction(float key1, float key2);
+        
+        // hash function
+        int hashFunction(float longitude, float latitude);
 };
 
-class node{
+class bucket{
     private:
         // class variables
-        float hashCode;
-        Restaurant rstrnt;
-        node *nextNode;
+        int hashCode; // hashcode identifying the bucket
+        std::vector<Restaurant> chainList; // vector of restaurant "chained" in this bucket
     public:
         // getters
-        float getHashCode();
-        Restaurant getRestaurant();
-        Node *getNextNode();
+        int getHashCode(); // returns hashcode integer
+        std::vector<Restaurant> getRestaurants(); // returns vector of "chained" restaurants stored in bucket
 
         // setters
-        void setRestaurant(Restuarant R);
-        void setHashCode(float code);
-        void setNextNode(Node* next);
+        void addRestaurant(Restaurant R); // adds a restuarant to bucket
+        void setHashCode(int code); // sets the hashcode for bucket
 
         // constructors
-        HashNode(float K, const Restaurant R);
+        bucket();
+        bucket(int code);
 };
-//#endif //PROJECT3_RESTAURANT_H
